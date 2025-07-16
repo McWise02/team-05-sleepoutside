8cf1be071233852e9dd120a5d30c8481bf54320c
McWise02 <micah02.brown@gmail.com>
2025-07-16 22:09:56 +0200

    team project 3

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -30,3 +30,44 @@ export function getParam(param) {
   return urlParams.get(param);
 }
 
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(template);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if(callback) {
+    callback(data);
+  }
+}
+
+async function loadTemplate(path) {
+  try {
+    const response = await fetch(path);
+    if (response.status !== 200) {
+      throw new Error(`Failed to load template from ${path}: ${response.statusText}`);
+    }
+    let template = await response.text();
+    return template;
+    }
+  catch (error) {
+    throw new Error(`Error loading template from ${path}: ${error.message}`);
+  }
+
+}
+
+export async function loadHeaderFooter () {
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const header = document.querySelector("#header");
+  const footer = document.querySelector("#footer");
+  renderWithTemplate(footerTemplate,footer);
+  renderWithTemplate(headerTemplate,header);
+  
+
+}
